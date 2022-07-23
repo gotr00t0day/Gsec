f1aefe7b778fac79093905ea0930e200db32d0d7
Miguel Sanchez <c0deninja@Luisas-MacBook-Air.local>
2022-07-23 19:45:11 -0400

    changed version number

--- gsec.py
+++ gsec.py
@@ -21,7 +21,7 @@ banner = f"""
 {Fore.RESET}
 
               ,~,
-             ((()-                   - GSec beta-v0.1
+             ((()-                   - GSec beta-v0.2
              -''-.                   - by c0deninja 
             (\  /\)                  - @gotr00t0day (Instagram)
       ~______\) | `\\
