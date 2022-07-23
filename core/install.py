a51c32d4943f73bf5666646c288a22af6602d8d0
Miguel Sanchez <c0deninja@Luisas-MacBook-Air.local>
2022-07-23 12:53:31 -0400

    bug fixes

--- core/install.py
+++ core/install.py
@@ -10,7 +10,3 @@ def commands(cmd):
 # This will install nuclei which does part of the vulnerability scanning.
 
 commands("go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest")
-
-# This will clone jaeles-signature to the Gsec directory
-
-commands("git clone https://github.com/jaeles-project/jaeles-signatures.git")
