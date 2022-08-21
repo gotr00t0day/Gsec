bb2ae654882ae8e5cf705e139e6c2421460f075f
Miguel Sanchez <c0deninja@Luisas-MacBook-Air.local>
2022-08-21 09:37:29 -0400

    added cors misconfiguration scanner

---
+++ vuln_db/corsmisconfig.py
@@ -0,0 +1,12 @@
+from colorama import Fore
+import requests
+
+
+def cors_scan(domain: str) -> str:
+    header = {'Origin': 'evil.com'}
+    r = requests.get(f"{domain}", verify=False, headers=header)
+    for keys, values in r.headers.items():
+        if keys == "Access-Control-Allow-Origin" and values == "evil.com":
+            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} CorsMisconfigutation: {Fore.YELLOW} POSSIBLE!") 
+        else:
+            pass
