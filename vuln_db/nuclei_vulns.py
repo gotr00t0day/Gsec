1a1fbfbba2549256e20c848f12515fba15cd8faa
Miguel Sanchez <c0deninja@Luisas-MacBook-Air.local>
2022-11-20 09:36:32 -0500

    added subproccess output to nuclei commands

--- vuln_db/nuclei_vulns.py
+++ vuln_db/nuclei_vulns.py
@@ -1,7 +1,6 @@
 from colorama import Fore
-from modules import scan
+from modules import scan, sub_output
 import requests
-import subprocess
 
 
 def nuclei_nginx_scan(url: str) -> str:
@@ -12,36 +11,24 @@ def nuclei_nginx_scan(url: str) -> str:
             scan.commands(f"nuclei -u {url} -t ~/nuclei-templates/misconfiguration/nginx/ -silent")
 
 def nuclei_cve_scan(domain: str) -> str:
-    cmd = f"nuclei -u {domain} -tags cve -severity critical,high -silent"
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out, err = p.communicate()
-    out = out.decode() 
-    print(out)
+    sub_output.subpro_scan(f"nuclei -u {domain} -tags cve -severity critical,high -silent")
 
-    cmd = f"nuclei -u {domain} -t cves/ -silent"
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out2, err = p.communicate()
-    out2 = out2.decode() 
-    print(out2)
+    sub_output.subpro_scan(f"nuclei -u {domain} -t cves/ -silent")
 
 def nuclei_fuzzing_scan(domain: str) -> str:
-    cmd = f"nuclei -t ~/nuclei-templates/fuzzing/ -u {domain} -silent"
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out, err = p.communicate()
-    out = out.decode() 
-    print(out)
+    sub_output.subpro_scan(f"nuclei -t ~/nuclei-templates/fuzzing/ -u {domain} -silent")
 
 def nuclei_ultimate_scan(domain: str) -> str:
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} CVE{Fore.WHITE} Status: {Fore.GREEN}Running...")
-    scan.commands(f"nuclei -u {domain} -tags cve -severity critical,high -silent")
-    scan.commands(f"nuclei -u {domain} -tags cve -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -tags cve -severity critical,high -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -tags cve -silent")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} CVE{Fore.WHITE} Status: {Fore.GREEN}DONE!\n")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} Vulnerabilities{Fore.WHITE} Status: {Fore.GREEN}Running...")
-    scan.commands(f"nuclei -u {domain} -t vulnerabilities/ -severity critical,high -silent")
-    scan.commands(f"nuclei -u {domain} -t vulnerabilities/ -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -t vulnerabilities/ -severity critical,high -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -t vulnerabilities/ -silent")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} Vulnerabilities{Fore.WHITE} Status: {Fore.GREEN}DONE!\n")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} Misconfigurations{Fore.WHITE} Status: {Fore.GREEN}Running...")
-    scan.commands(f"nuclei -u {domain} -t misconfiguration/ -severity critical,high -silent")
-    scan.commands(f"nuclei -u {domain} -t misconfiguration/ -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -t misconfiguration/ -severity critical,high -silent")
+    sub_output.subpro_scan(f"nuclei -u {domain} -t misconfiguration/ -silent")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Task{Fore.CYAN}:{Fore.LIGHTBLUE_EX} Misconfiguration{Fore.WHITE} Status: {Fore.GREEN}DONE!\n")
     print(f"{Fore.MAGENTA}[+] {Fore.CYAN}- {Fore.WHITE}Status: {Fore.GREEN}All Tasks done!!")
