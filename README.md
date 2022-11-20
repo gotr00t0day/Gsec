8a4b63f3809b02ea44dda0f44813dcdae79fc22d
Miguel Sanchez <c0deninja@Luisas-MacBook-Air.local>
2022-11-19 22:18:31 -0500

    new version updates

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Web Security Scanner
 
 
               ,~,
-             ((()-                   - GSec v0.16
+             ((()-                   - GSec v0.17
              -''-.                   - by c0deninja 
             (\  /\)                  - @gotr00t0day (Instagram)
       ~______\) | `\
--- gsec.py
+++ gsec.py
@@ -21,7 +21,7 @@ banner = f"""
 {Fore.RESET}
 
               ,~,
-             ((()-                   - GSec beta-v0.16
+             ((()-                   - GSec beta-v0.17
              -''-.                   - by c0deninja 
             (\  /\)                  - @gotr00t0day (Instagram)
       ~______\) | `\\
