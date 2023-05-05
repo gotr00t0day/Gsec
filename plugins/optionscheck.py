9d4d6009f3fba3cbb60a987c107c7108337efc40
Miguel Sanchez <c0deninja@Luisa-D.local>
2023-05-05 18:48:09 -0400

    bug fixes

--- plugins/optionscheck.py
+++ plugins/optionscheck.py
@@ -7,8 +7,14 @@ header = {"User-Agent": user_agent_}
 
 def Get_Options(url: str) -> str:
     r = requests.options(f"{url}", verify=False, headers=header)
+    allowed = []
     for item, value in r.headers.items():
         if "Allow" in item:
-            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} OPTIONS: {Fore.GREEN}{value}")
+            allowed.append(value)
         else:
             pass
+    if allowed:
+        allowed = ", ".join(allowed)
+        print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} OPTIONS: {Fore.GREEN}{allowed}")
+    else:
+        pass
