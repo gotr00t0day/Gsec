9f8b921fb2161d77683b635f47499d2270024e72
Miguel Sanchez <c0deninja@Luisa-D.local>
2023-05-13 15:25:14 -0400

    changed a few things

--- modules/fetch_requests.py
+++ modules/fetch_requests.py
@@ -16,18 +16,26 @@ header = {"User-Agent": user_agent_}
 
 def do_requests(url: str, proxy = None) -> str:
     try:
-        if proxy is None:
-            sessions = requests.Session()      
-            res = sessions.get(url, verify=False, headers=header, allow_redirects=True)
-            test.get_response(url, res)
+        sessions = requests.Session()      
+        res = sessions.get(url, verify=False, headers=header, allow_redirects=True)
+        if res.status_code == 200:
+            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} {url} {Fore.GREEN}200")
+        elif res.status_code == 403:
+            soup = BeautifulSoup(res.text, 'html.parser')
+            title = soup.find("title")
+            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} {url} {Fore.RED} Forbidden ({title.get_text()})")
+        elif res.status_code == 404:
+            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} {url} {Fore.RED} 404")
+            print(f"{Fore.RED} EXITING!!")
+            sys.exit()
+        elif res.history == 301 or 302:
+            location = []
+            for key, desc in res.headers.items():
+                if "Location" in key or "location" in key:
+                    location.append(desc)
+            print(f"{Fore.MAGENTA}[+] {Fore.CYAN}-{Fore.WHITE} {url} {Fore.RED} seems to be redirecting to {Fore.CYAN}{res.url}")
         else:
-            sessions = requests.Session()      
-            proxies = {
-                'sock5': "199.229.254.129"
-            }
-            res = sessions.get(url, verify=False, headers=header, allow_redirects=True, proxies=proxies)
-            test.get_response(url, res)
-
+            print(f"{url} {res.status_code}")
     except requests.exceptions.InvalidSchema:
         print("Please use https://www.target.com")
     except requests.exceptions.ConnectionError:
